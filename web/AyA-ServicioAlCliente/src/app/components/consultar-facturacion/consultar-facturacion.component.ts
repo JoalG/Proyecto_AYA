821693023122511760be62ff0c2c85c363e66f18
Jose Gamboa <joalgamro2010@gmail.com>
2022-03-24 15:29:14 -0600

    U: Consultar facturacion component

--- web/AyA-ServicioAlCliente/src/app/components/consultar-facturacion/consultar-facturacion.component.ts
+++ web/AyA-ServicioAlCliente/src/app/components/consultar-facturacion/consultar-facturacion.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { identity } from 'rxjs';
+import { CustomResponse } from 'src/app/models/custom-response.model';
+import { BillsService } from 'src/app/services/bills.service';
 
 @Component({
   selector: 'app-consultar-facturacion',
@@ -8,15 +11,18 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class ConsultarFacturacionComponent implements OnInit {
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private readonly _billService: BillsService
+    ) { }
 
   ngOnInit(): void {
   }
 
   myForm: FormGroup = this.fb.group({
-    nisNumber: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(7)]],
-    idType: ['', [Validators.required]],
-    idNumber: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]]
+    nis: ['3011939', [Validators.required, Validators.minLength(7), Validators.maxLength(7)]],
+    clientIdType: ['Cédula de identidad', [Validators.required]],
+    clientId: ['123213231', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]]
   });
 
   isNumberKey(evt:KeyboardEvent) {
@@ -25,6 +31,19 @@ export class ConsultarFacturacionComponent implements OnInit {
         return false;
     return true;
     
+  }
+
+  async getCollectionBill(){
+    let nis = this.myForm.get('nis')?.value;
+    let clientIdType = this.myForm.get('clientIdType')?.value;
+    let clientId = this.myForm.get('clientId')?.value;
+    let bill = (await this._billService.getCollectionBill(nis, clientIdType, clientId).toPromise());
+    if(bill!.success){
+      console.log(bill?.data)
+    }
+    else{
+      console.log(bill?.message)
+    }
   }
 
 }
