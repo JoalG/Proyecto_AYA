c1096c919b9c2bbc2723c09c09ead1ea62197182
Jose Gamboa <joalgamro2010@gmail.com>
2022-03-24 19:46:42 -0600

    A: Routing

--- web/AyA-ServicioAlCliente/src/app/components/consultar-facturacion/consultar-facturacion.component.ts
+++ web/AyA-ServicioAlCliente/src/app/components/consultar-facturacion/consultar-facturacion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { identity } from 'rxjs';
 import { CustomResponse } from 'src/app/models/custom-response.model';
 import { BillsService } from 'src/app/services/bills.service';
@@ -13,16 +14,17 @@ export class ConsultarFacturacionComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private readonly _billService: BillsService
+    private readonly _billService: BillsService,
+    private router: Router
     ) { }
 
   ngOnInit(): void {
   }
 
   myForm: FormGroup = this.fb.group({
-    nis: ['3011939', [Validators.required, Validators.minLength(7), Validators.maxLength(7)]],
-    clientIdType: ['Cédula de identidad', [Validators.required]],
-    clientId: ['123213231', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]]
+    nis: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(7)]],
+    clientIdType: ['', [Validators.required]],
+    clientId: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]]
   });
 
   isNumberKey(evt:KeyboardEvent) {
@@ -38,8 +40,11 @@ export class ConsultarFacturacionComponent implements OnInit {
     let clientIdType = this.myForm.get('clientIdType')?.value;
     let clientId = this.myForm.get('clientId')?.value;
     let bill = (await this._billService.getCollectionBill(nis, clientIdType, clientId).toPromise());
-    if(bill!.success){
+    console.log(bill)
+    if(bill!.success==true){
       console.log(bill?.data)
+      this._billService.set_sharingBill(bill?.data);
+      this.router.navigate(['/detalles-facturacion'])
     }
     else{
       console.log(bill?.message)
