ab5fab20189e78deb4ca3cb635c5a47ef4b3378c
Joaquin <joaquinenrique2010@gmail.com>
2022-03-30 18:47:24 -0600

    C: reporteAveria model

---
+++ web/AyA-ServicioAlCliente/src/app/models/reporte-averia.ts
@@ -0,0 +1,14 @@
+export interface ReporteAveria{
+    _id: string,
+    provincia: string,
+    canton: string,
+    distrito: string,
+    nis: string,
+    name: string,
+    lastname: string,
+    phoneNumber: string,
+    email: string,
+    description: string,
+    type: number,
+    state: number
+}
