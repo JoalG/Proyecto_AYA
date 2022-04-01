474d3c1961faf735b5faad3ea46546714113e604
Joaquin <joaquinenrique2010@gmail.com>
2022-04-01 11:14:11 -0600

    A: user-main-page to user-tramites-solicitudes

--- web/AyA-ServicioAlCliente/src/app/components/users/user-tramites-solicitudes/user-tramites-solicitudes.component.ts
+++ web/AyA-ServicioAlCliente/src/app/components/users/user-tramites-solicitudes/user-tramites-solicitudes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-tramites-solicitudes',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserTramitesSolicitudesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  goToTramitesSolicitudes(){
+    this.router.navigate(['/list-reporte-averia']);
   }
 
 }
