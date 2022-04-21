c11574d8a401baf3fe0d1a7fbb896190de96dd2b
Joaquin <joaquinenrique2010@gmail.com>
2022-04-20 20:55:14 -0600

    C: frontend suspension model

---
+++ web/AyA-ServicioAlCliente/src/app/models/suspension.ts
@@ -0,0 +1,9 @@
+export interface Suspension{
+    _id?: string,
+    provincia: string,
+    canton: string,
+    fechaInit: Date,
+    fechaFin: Date,
+    description: string,
+    deleted: boolean
+}
