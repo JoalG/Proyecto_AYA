e2c881a63b42cb7d681bc4eb8c97171012563689
Joaquin <joaquinenrique2010@gmail.com>
2022-04-23 14:41:24 -0600

    A: getPayments

--- web/AyA-ServicioAlCliente/src/app/services/payment.service.ts
+++ web/AyA-ServicioAlCliente/src/app/services/payment.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CustomResponse } from '../models/custom-response.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentService {
 
-  constructor() { }
+  URL_API = 'http://localhost:3000/payments';
+
+  constructor(
+    private http:HttpClient, 
+    private router: Router
+  ) { }
+
+  getPayments(nis: string, clientIdType: string, clientId: string):Observable<CustomResponse>{
+    return this.http.get<CustomResponse>(`${this.URL_API}/${nis}&${clientIdType}&${clientId}`);
+  }
 }
