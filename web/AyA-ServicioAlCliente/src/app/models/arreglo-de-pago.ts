235d8040adec3963d5b13bcd0309f0431537c6c8
Joaquin <joaquinenrique2010@gmail.com>
2022-05-23 20:30:04 -0600

    A: ArregloDePago model

---
+++ web/AyA-ServicioAlCliente/src/app/models/arreglo-de-pago.ts
@@ -0,0 +1,12 @@
+export interface ArregloDePago {
+    _id?: string
+    nis: string,
+    clientName: string,
+    date?: Date,
+    state?: string,
+    type?: string,
+    telephone: string,
+    cellphone: string,
+    email: string,
+    observations: string,
+}
