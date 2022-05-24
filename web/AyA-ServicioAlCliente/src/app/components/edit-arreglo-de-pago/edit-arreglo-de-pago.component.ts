59db34ab705c8592b24aad6d5d27a8a1f7b8092a
Joaquin <joaquinenrique2010@gmail.com>
2022-05-24 10:17:06 -0600

    C: edit-arreglo-de-pago component

---
+++ web/AyA-ServicioAlCliente/src/app/components/edit-arreglo-de-pago/edit-arreglo-de-pago.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-arreglo-de-pago',
+  templateUrl: './edit-arreglo-de-pago.component.html',
+  styleUrls: ['./edit-arreglo-de-pago.component.css']
+})
+export class EditArregloDePagoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
