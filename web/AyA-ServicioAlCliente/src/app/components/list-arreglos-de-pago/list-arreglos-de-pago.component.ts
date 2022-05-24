336ba70236ed4c036c30cbf5e461bb68c69d92c0
Joaquin <joaquinenrique2010@gmail.com>
2022-05-23 20:47:56 -0600

    A: list-arreglos-de-pago

--- web/AyA-ServicioAlCliente/src/app/components/list-arreglos-de-pago/list-arreglos-de-pago.component.ts
+++ web/AyA-ServicioAlCliente/src/app/components/list-arreglos-de-pago/list-arreglos-de-pago.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TramitesService } from 'src/app/services/tramites.service';
+
 
 @Component({
   selector: 'app-list-arreglos-de-pago',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListArreglosDePagoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly _tramitesService: TramitesService,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
+
+  tramites!: any[]
 
   ngOnInit(): void {
+    this.getTramites();
   }
 
+  async getTramites(){
+    let res = (await this._tramitesService.getTramites().toPromise());
+    if(res?.success){
+      this.tramites = res?.data;
+    }
+    else{
+      console.log(res?.message);
+    }
+  }
+
+  goToEditTramite(id: string, type: string){
+    switch (type) {
+      case "Arreglo de pago":
+        //this.router.navigate(['/edit-arreg', id]);
+        break;
+    
+      default:
+        break;
+    }
+  }
 }
